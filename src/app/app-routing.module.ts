85da0ca67cedb0af67baf407e1f030dcfc136c0f
Pedri <pedri6891@gmail.com>
2019-03-21 15:46:15 -0400

    Creando el side Bar y el area de trabajo a la derecha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ObraListComponent} from "./modules/Obra/obra-list/obra-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'obras', component:ObraListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
